db91645a68005cd84f4a04b2083bc5e8e188a1ca
John Josiah <John.J@bullseye.local>
2023-05-26 13:45:01 +0100

    sending

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -17,12 +18,20 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
 
     else: 
         form = UserRegisterForm()
     return render(request, 'users/register.html',{'form': form})
+
+# a decorator add fuctionality to an existing function
+# an in these case it add functionality to our profile view where a user must login to view the profile
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
+
 
 
 
